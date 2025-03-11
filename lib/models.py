67e2decdba419da5e0cb6c67c08470b7d304ed1a
Symon.Dev <92350374+simon036@users.noreply.github.com>
2025-03-11 13:51:38 +0300

    Complete challange

--- lib/models.py
+++ lib/models.py
@@ -1,70 +1,79 @@
+```python
+# SQLAlchemy modules importation 
 from sqlalchemy import ForeignKey, Column, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base, sessionmaker
 from sqlalchemy import create_engine
 
+# Defining the base class for declarative models
 Base = declarative_base()
 
+# Defining the Audition model representing auditions for roles
 class Audition(Base):
     __tablename__ = "auditions"
 
     id = Column(Integer, primary_key=True)
-    actor = Column(String)
-    location = Column(String)
-    phone = Column(Integer)
-    hired = Column(Boolean, default=False) 
-    role_id = Column(Integer, ForeignKey("roles.id"))
+    actor = Column(String)  # Name of the actor auditioning
+    location = Column(String)  # Location of the audition
+    phone = Column(Integer)  # Contact phone number of the actor
+    hired = Column(Boolean, default=False)  # Whether the actor was hired
+    role_id = Column(Integer, ForeignKey("roles.id"))  # Foreign key to the Role model
 
-    role = relationship("Role", back_populates="auditions")
+    role = relationship("Role", back_populates="auditions")  # Relationship to the Role model
 
     def call_back(self):
-        self.hired = True
+        self.hired = True  # Method to mark the actor as hired
 
+# Defining the Role model representing roles in a theater production
 class Role(Base):
     __tablename__ = "roles"
 
     id = Column(Integer, primary_key=True)
-    character_name = Column(String)
+    character_name = Column(String)  # Name of the character in the role
 
-    auditions = relationship("Audition", back_populates="role")
+    auditions = relationship("Audition", back_populates="role")  # Relationship to the Audition model
 
     def actors(self):
-        return [audition.actor for audition in self.auditions]
-    
+        return [audition.actor for audition in self.auditions]  # Returns a list of actors auditioning for this role
+
     def locations(self):
-        return [audition.location for audition in self.auditions]
-    
+        return [audition.location for audition in self.auditions]  # Returns a list of audition locations for this role
+
     def lead(self):
         hired_auditions = [audition for audition in self.auditions if audition.hired]
-        return hired_auditions[0] if hired_auditions else "no actor has been hired for this role."
-    
+        return hired_auditions[0] if hired_auditions else "no actor has been hired for this role."  # Returns the hired lead actor or a message if none
+
     def understudy(self):
         hired_auditions = [audition for audition in self.auditions if audition.hired]
-        return hired_auditions[1] if len(hired_auditions) > 1 else "no actor has been hired for understudy for this role."
+        return hired_auditions[1] if len(hired_auditions) > 1 else "no actor has been hired for understudy for this role."  # Returns the hired understudy or a message if none
 
-
+# Creating an SQLite database engine and creating all tables
 engine = create_engine("sqlite:///theater.db")
 Base.metadata.create_all(engine)
 
-
+# Creating a session to interact with the database
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# Creating a new role and two auditions for that role
 role = Role(character_name="Simon")
-
 audition1 = Audition(actor="Morgan Freeman", location="L.A California", phone=123456789, role=role)
 audition2 = Audition(actor="Denzel Washington", location="L.A California", phone=987654321, role=role)
 
+# Adding the role and auditions to the session and commiting to the database
 session.add(role)
 session.add(audition1)
 session.add(audition2)
 session.commit()
 
-
+# Printing list of actors and locations for the role
 print(role.actors())  
 print(role.locations())
 
+# Marking the first audition as hired and commit the change
 audition1.call_back()
 session.commit()
 
+# Printing the lead actor and understudy for the role
 print(role.lead())  
-print(role.understudy())  
+print(role.understudy()) 
+```
